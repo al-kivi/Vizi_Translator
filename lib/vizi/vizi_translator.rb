29ad169c760c979047795074041a73e421c51467
alkivi <alkivi@alkivi.(none)>
2012-08-10 18:49:50 -0400

    Modified the translation module to split the file for translation.

--- lib/vizi/vizi_translator.rb
+++ lib/vizi/vizi_translator.rb
@@ -52,29 +52,47 @@ module Vizi
         end
       end
       input_text = new_array.join
-      input_text = URI.escape(input_text)    
-      p input_text
-      begin
-        raise UnSupportedLanguage unless SUPPORTED_LANG_CODES.include?(output_code)
-        site_url = 'www.syslang.com'
-        uri_method = '/frengly/controller?'
-        uri_string = 'action='+default_action+'&src='+default_src+'&dest='+output_code+'&text='+input_text+'&username='+username+'&password='+password
-        response = Net::HTTP.get_response(site_url, uri_method + uri_string)
-        if response.code == "200"
-          xml_data = response.body
-          data = XmlSimple.xml_in(xml_data)
-          result = data["translation"][0]
-          chash.each {|key, value| result.gsub!(key, value) }
-          result
-        else
-          puts response.body
-          raise StandardError, response.body
+      # added new logic to split the file
+      if input_text.length > 500 and not input_text.index("$-")
+        for i in 0..input_text.length/500-1
+          x = input_text[0..500*(i+1)].rindex(/\n/)
+          input_text.insert(x+1, '$-')
         end
-        rescue UnSupportedLanguage
-          raise UnSupportedLanguage.new
-        rescue => err_msg
-          puts "#{err_msg}"
+      end 
+      input_text = URI.escape(input_text)
+      input_array = Array.new
+      if input_text.index("$-")
+        input_array = input_text.split("$-")
+      else
+        input_array[0] = input_text
       end
+      # end of splitter logic
+      result_all = ""
+      for i in 0..input_array.length-1
+        begin
+          sleep 3 if i > 0
+          raise UnSupportedLanguage unless SUPPORTED_LANG_CODES.include?(output_code)
+          site_url = 'www.syslang.com'
+          uri_method = '/frengly/controller?'
+          uri_string = 'action='+default_action+'&src='+default_src+'&dest='+output_code+'&text='+input_array[i]+'&username='+username+'&password='+password
+          response = Net::HTTP.get_response(site_url, uri_method + uri_string)
+          if response.code == "200"
+            xml_data = response.body
+            data = XmlSimple.xml_in(xml_data)
+            result = data["translation"][0]
+            chash.each {|key, value| result.gsub!(key, value) }
+            result_all << result
+          else
+            puts response.body
+            raise StandardError, response.body
+          end
+          rescue UnSupportedLanguage
+            raise UnSupportedLanguage.new
+          rescue => err_msg
+            puts "#{err_msg}"
+        end
+      end
+      result_all
     end
 
   end # Class 
